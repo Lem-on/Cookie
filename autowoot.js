cd0bee4a8e60fa9a6e81b8a7c39cfe69adbc82bc
Lem-on <petr.smoldas@centrum.cz>
2015-08-19 09:45:06 +0200

    Update autowoot.js

--- autowoot.js
+++ autowoot.js
@@ -5,5 +5,3 @@ API.on(API.ADVANCE, aw); /* autowoot */
 function aw(){ 
 	$("#woot").click(); 
 }
-/* ahoj */
-if(API.on(API.CHAT, a); function a(b){ if(b.message == "ahoj"){ API.sendChat("ahoj"); }}
