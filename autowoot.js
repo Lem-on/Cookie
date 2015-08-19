b6ce865eb01d5797c4e0bc0b7cccf691a51f6399
Lem-on <petr.smoldas@centrum.cz>
2015-08-19 09:43:52 +0200

    Update autowoot.js

--- autowoot.js
+++ autowoot.js
@@ -1,11 +1,9 @@
 API.chatLog("Cookie Loaded"); 
 API.on(API.ADVANCE, aw); /* autowoot */
-API.on(API.ADVANCE, lol); /* ahoj */
 
 /* autowoot */
 function aw(){ 
 	$("#woot").click(); 
 }
 /* ahoj */
-API.on(API.CHAT, a); function a(b){ if(b.message == "ahoj"){ API.sendChat("ahoj"); } }
-}
+if(API.on(API.CHAT, a); function a(b){ if(b.message == "ahoj"){ API.sendChat("ahoj"); }}
