6780f52f6c24db86916ec666c2aaecd6736ac611
Lem-on <petr.smoldas@centrum.cz>
2015-08-20 22:00:44 +0200

    Update autowoot.js

--- autowoot.js
+++ autowoot.js
@@ -1,6 +1,7 @@
 API.chatLog("Cookie Loaded"); 
 API.on(API.ADVANCE, aw); /* autowoot */
 API.on(API.CHAT, lol);  /* lemon */
+API.on(API.CHAT, lol);  /* ahoj */
 
 /* autowoot */
 function aw(){ 
@@ -9,4 +10,8 @@ function aw(){
 
 
 /* lemon */
-function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("lemon") > -1){ API.sendChat("Lemon HYPE"); } } }
+function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("lemon") > -1){ API.sendChat(":lemon:Lemon HYPE:lemon:"); } } }
+
+/* ahoj*/
+
+function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("ahoj") < 0 &&  if(msg.toLowerCase().indexOf("čau")  > -1){ API.sendChat("Ahoj :yum:") < 0); } } }
