37d8bb71abc65c07522a955f13661fc5cf914f3e
Lem-on <petr.smoldas@centrum.cz>
2015-08-20 22:29:46 +0200

    Update lemonscript.js

--- lemonscript.js
+++ lemonscript.js
@@ -1,16 +1,28 @@
-API.chatLog("Cookie Loaded"); 
+
+
+API.chatLog("Cookie Loaded");
 API.on(API.ADVANCE, aw); /* autowoot */
-API.on(API.CHAT, lol);  /* lemon */
-
-
+API.on(API.CHAT, lol); /* lemon */
+ 
+ 
 /* autowoot */
-function aw(){ 
-	$("#woot").click(); 
+ 
+function aw() {
+    $("#woot").click();
 }
-
-
+ 
+ 
 /* lemon */
-function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("lemon") > -1){ API.sendChat(":lemon:Lemon HYPE:lemon:"); } } }
-function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("ahoj") > -1){ API.sendChat("Ahoj :yum:") < 0); } } }
-function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("čau") > -1){ API.sendChat("Ahoj :yum:") < 0); } } }
-function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("čus") > -1){ API.sendChat("Ahoj :yum:") < 0); } } }
+ 
+function lol(a) {
+    var me = API.getUser().id;
+    var msg = a.message;
+    var from = a.uid;
+    if (me != from) {
+        if (msg.toLowerCase().indexOf("lemon") > -1) {
+            API.sendChat(":lemon:Lemon HYPE:lemon:");
+        }
+        if (msg.toLowerCase().indexOf("ahoj") > -1 || msg.toLowerCase().indexOf("čau") > -1 || msg.toLowerCase().indexOf("čus") > -1 ||  ) {
+             API.sendChat("Ahoj :yum:");
+        }
+    }
