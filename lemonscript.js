8cda322e463de090bbbbfe9f782294b9b9dca205
Lem-on <petr.smoldas@centrum.cz>
2015-08-20 22:23:01 +0200

    Update lemonscript.js

--- lemonscript.js
+++ lemonscript.js
@@ -1,9 +1,6 @@
 API.chatLog("Cookie Loaded"); 
 API.on(API.ADVANCE, aw); /* autowoot */
 API.on(API.CHAT, lol);  /* lemon */
-API.on(API.CHAT, lol);  /* ahoj */
-API.on(API.CHAT, lol);  /* čau */
-API.on(API.CHAT, lol);  /* čus */
 
 
 /* autowoot */
@@ -14,12 +11,6 @@ function aw(){
 
 /* lemon */
 function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("lemon") > -1){ API.sendChat(":lemon:Lemon HYPE:lemon:"); } } }
-
-/* ahoj */
 function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("ahoj") > -1){ API.sendChat("Ahoj :yum:") < 0); } } }
-
-/* čau */
 function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("čau") > -1){ API.sendChat("Ahoj :yum:") < 0); } } }
-
-/* čus */
 function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("čus") > -1){ API.sendChat("Ahoj :yum:") < 0); } } }
