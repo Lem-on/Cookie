de566856a3fda37e22e784657c6b0e022e9e7de3
Lem-on <petr.smoldas@centrum.cz>
2015-08-20 23:06:42 +0200

    Update lemonscript.js

--- lemonscript.js
+++ lemonscript.js
@@ -19,13 +19,13 @@ function lol(a) {
     var msg = a.message;
     var from = a.uid;
     if (me != from) {
-        if (msg.toLowerCase().indexOf("lemon") > -1) {
+        if (msg.toLowerCase().indexOf("@Kotletka lemon") > -1) {
             API.sendChat(":lemon:Lemon HYPE:lemon:");
         }
-        if (msg.toLowerCase().indexOf("ahoj") > -1 || msg.toLowerCase().indexOf("čau") > -1 || msg.toLowerCase().indexOf("čus") > -1 ) {
+        if (msg.toLowerCase().indexOf("@Kotletka ahoj") > -1 || msg.toLowerCase().indexOf("@Kotletka čau") > -1 || msg.toLowerCase().indexOf("@Kotletka čus") > -1 ) {
              API.sendChat("Ahoj :yum:");
         }
-        if (msg.toLowerCase().indexOf("infopravidla") > -1) {
+        if (msg.toLowerCase().indexOf("@Kotletka infopravidla") > -1) {
              API.sendChat("Nespamovat | Nedělat reklamy | Nežádat o funkci | Chovat se a psát slušně | Nevhodná hudba/video = skip | Příliš mehů = skip | Max. délka 10 min.");
         }
     }
