16ae00e93ee151f535f7352b8aeb5e3692c820ac
Lem-on <petr.smoldas@centrum.cz>
2015-08-20 22:05:41 +0200

    Update lemonscript.js

--- lemonscript.js
+++ lemonscript.js
@@ -1,7 +1,7 @@
 API.chatLog("Cookie Loaded"); 
 API.on(API.ADVANCE, aw); /* autowoot */
 API.on(API.CHAT, lol);  /* lemon */
-API.on(API.CHAT, lol);  /* ahoj */
+
 
 /* autowoot */
 function aw(){ 
@@ -12,5 +12,4 @@ function aw(){
 /* lemon */
 function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("lemon") > -1){ API.sendChat(":lemon:Lemon HYPE:lemon:"); } } }
 
-/* ahoj*/
-function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("ahoj") < 0 &&  if(msg.toLowerCase().indexOf("čau")  > -1){ API.sendChat("Ahoj :yum:") < 0); } } }
+function lol(a){ var me = API.getUser().id; var msg = a.message; var from = a.uid; if(me != from){ if(msg.toLowerCase().indexOf("ahoj") > -1){ API.sendChat("Ahoj :yum:") < 0); } } }
