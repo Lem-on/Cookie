b429a2081377055afb46225a4e80db2acc7291bd
Lem-on <petr.smoldas@centrum.cz>
2015-08-28 22:06:36 +0200

    Update lemonscript.js

--- lemonscript.js
+++ lemonscript.js
@@ -20,20 +20,20 @@ function lol(a) {
         if (msg.toLowerCase().indexOf("cookie lemon") > -1) {
             API.sendChat(":lemon:Lemon HYPE:lemon:");
         }
-        if (msg.toLowerCase().indexOf("cookie ahoj") > -1 || msg.toLowerCase().indexOf("cookie čau") > -1 || msg.toLowerCase().indexOf("cookie čus") > -1 ) {
-             API.sendChat("Ahoj :yum:");
+        if (msg.toLowerCase().indexOf("kotletka ahoj") > -1 || msg.toLowerCase().indexOf("kotletka čau") > -1 || msg.toLowerCase().indexOf("kotletka čus") > -1 ) {
+             API.sendChat("@"+a.un+ Ahoj :yum:");
         }
         if (msg.toLowerCase().indexOf("proč?") > -1 || msg.toLowerCase().indexOf("proč") > -1 || msg.toLowerCase().indexOf("A proč?") > -1 ) {
-             API.sendChat("Protože.. PROTO!");
+             API.sendChat("@"+a.un+ Protože.. PROTO!");
         }
         if (msg.toLowerCase().indexOf("mám tě rád") > -1 || msg.toLowerCase().indexOf("miluji tě") > -1 || msg.toLowerCase().indexOf("nesnáším tě") > -1 ) {
-             API.sendChat("Já tebe taky :heart_decoration:");
+             API.sendChat("@"+a.un+ Já tebe taky :heart_decoration:");
         }
         if (msg.toLowerCase().indexOf("si holka") > -1 || msg.toLowerCase().indexOf("dáme sex") > -1 || msg.toLowerCase().indexOf("si bot") > -1 ) {
-             API.sendChat("To je ve hvězdách");
+             API.sendChat("@"+a.un+ To je ve hvězdách");
         }
-            if (msg.toLowerCase().indexOf("cookie :D") > -1) {
-            API.sendChat("Nesměj se mi :(");
+            if (msg.toLowerCase().indexOf("kotletka sežeru") > -1) {
+            API.sendChat("@"+a.un+ Když jinak nedáš :3");
         }
     }
 }
