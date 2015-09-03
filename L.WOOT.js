9b66ebf4bf92596fb2c7bd85e75c782a40c90406
Lem-on <petr.smoldas@centrum.cz>
2015-09-03 15:15:34 +0200

    Create L.WOOT.js

---
+++ L.WOOT.js
@@ -0,0 +1,11 @@
+API.chatLog("L.WOOT v6.9 :banana:");
+API.on(API.ADVANCE, aw); /* autowoot */
+API.on(API.CHAT, lol); /* lemon */
+ 
+ 
+/* autowoot */
+ 
+function aw() {
+    $("#woot").click();
+}
+ 
