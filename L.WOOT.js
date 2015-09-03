6443a7562f234d45f4e6d055905594ed06ce1361
Lem-on <petr.smoldas@centrum.cz>
2015-09-03 16:06:42 +0200

    Update L.WOOT.js

--- L.WOOT.js
+++ L.WOOT.js
@@ -1,4 +1,4 @@
-API.chatLog("/me L.WOOT v6.9 [Banana :banana:] Loaded :white_check_mark:");
+API.chatLog("/me L.WOOT v6.9 "Banana" :banana:] Loaded :white_check_mark:");
 API.on(API.ADVANCE, aw); /* autowoot */
  
  
