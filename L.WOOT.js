35eca0c61806819b5fed2e17373f945b513eb3e1
Lem-on <petr.smoldas@centrum.cz>
2015-09-30 07:50:22 +0200

    Update L.WOOT.js

--- L.WOOT.js
+++ L.WOOT.js
@@ -1,10 +1,10 @@
-API.chatLog("/me L.WOOT v6.9 "Banana" :banana:] Loaded :white_check_mark:");
+API.chatLog("L.WOOT v6.9 "Banana"] Loaded");
 API.on(API.ADVANCE, aw); /* autowoot */
  
  
 /* autowoot */
  
 function aw() {
-    $("#woot").click();
+    $("#icon-arrow-up").click();
 }
  
